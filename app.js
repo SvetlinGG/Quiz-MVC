942ef5d4f0b92b52445f8e9301d1e6f8783e28a7
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-25 13:04:36 +0200

    setup engine -- handlebars

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ const router = require('./routes/index,js');
 const app = express();
 const port = 6000;
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.engine('hbs', engine({
+    extname: '.hbs',
+    defaultLayout: 'layout',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials')
+}));
+
+
 
 app.listen(port, () => console.log(`Quiz MVC running on http://localhost:${port}`));
 
