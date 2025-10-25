dcc7c3ee178fd8c04f6bdbae52bf967655873e43
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-25 14:07:33 +0200

    controllers logic

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -20,4 +20,38 @@ exports.showQuiz = (req, res) => {
         quizId: quiz.id,
         questions: shuffledQuestions
     });
-}
+};
+
+exports.submitQuiz = (req, res) => {
+    const quiz = getQuizById(req.params.id);
+
+    if (!quiz) return res.status(404).send('Quiz not found');
+
+    const userAnswer = req.body || {};
+    let correct = 0;
+
+    const details = quiz.questions.map((q, i) => {
+        const user = userAnswer[`q_${q.id}`];
+        const isCorrect = user === q.answer;
+
+        if (isCorrect) correct ++;
+        return {
+            index: i + 1,
+            question: q.text,
+            user,
+            correctAnswer: q.answer,
+            isCorrect
+        };
+    });
+
+    res.render('results', {
+        title: quiz.title,
+        quizId: quiz.id,
+        total: quiz.questions.length,
+        correct,
+        details,
+        scoreMsg: correct === quiz.questions.length
+          ? 'Great job'
+          : (correct >= quiz.questions.length * 0.6 ? 'Nice try!' : 'Try again!')
+    });
+};
