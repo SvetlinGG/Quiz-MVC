402f1eab8f36da47036cd0b8e0fcb9b23ec87354
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-26 09:42:27 +0100

    functionality of retake quiz button

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,6 @@ const router = express.Router();
 router.get('/', quizController.home);
 router.get('/quiz/:id', quizController.showQuiz);
 router.post('/quiz/:id/submit', quizController.submitQuiz);
-router.post('/quiz/:id/retake', (req, res) => res.redirect(`quiz/${req.params.id}`));
+router.post('/quiz/:id/retake', (req, res) => res.redirect(`/quiz/${req.params.id}`));
 
 module.exports = router
